aeb26f8fc1a6f5ae063034dd3829f321ac29eaab
acodes189-png <acodes189@gmail.com>
2026-05-26 19:51:32 +0530

    added background and improved the main scene graphically

--- Scenes/phone ui/phone_ui.gd
+++ Scenes/phone ui/phone_ui.gd
@@ -1,5 +1,5 @@
 extends Node
 
 func _process(delta):
-	if Input.is_action_just_pressed("back to main"):
+	if Input.is_action_just_pressed("back_to_main"):
 		get_tree().change_scene_to_file("res://Scenes/Game/main.tscn")
