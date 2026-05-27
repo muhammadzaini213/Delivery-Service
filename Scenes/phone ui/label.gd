e9511424717e157d87b1f1c007303fde7f80169f
Muhammad Zaini <163093916+muhammadzaini213@users.noreply.github.com>
2026-05-27 17:35:19 +0800

    Phone UI (#15)
    * finished phoneUI
    
    * downloaded font
    
    ---------
    
    Co-authored-by: acodes189-png <acodes189@gmail.com>

--- Scenes/phone ui/label.gd
+++ Scenes/phone ui/label.gd
@@ -1,13 +1,34 @@
 extends Label
+
 var flat = [101,102,103,201,202,203,301,302,303,401,402,403,501,502,503]
 @export var deliver: int = 101
+var receivingOrder: bool = false
 
+@onready var my_button = $"../CheckButton"
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	deliver = flat.pick_random()
-	$".".text = "Order Recieved from "+str(deliver)
-	while true:
-		await get_tree().create_timer(30.0).timeout
+	# Connect the signal manually
+	my_button.toggled.connect(_on_check_button_toggled)
+	# Set initial text
+	text = "not receiving orders"
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	receivingOrder = toggled_on
+	
+	if receivingOrder:
+		start_order_cycle()
+	else:
+		text = "not receiving orders"
+
+func start_order_cycle() -> void:
+	# This loop runs as long as receivingOrder is true
+	while receivingOrder:
 		deliver = flat.pick_random()
-		$".".text = "Order Recieved from "+str(deliver)
+		text = "Order Recieved from " + str(deliver)
+		
+		# Wait for 30 seconds before the next order
+		await get_tree().create_timer(30.0).timeout
+		
+		# If the user toggled it OFF during the 30s wait, break the loop
+		if not receivingOrder:
+			break
